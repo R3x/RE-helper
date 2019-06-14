f81b2810c5817c2db88c3a6ca80161a9189d88b4
Siddharth M <siddharth.muralee@gmail.com>
2019-06-14 11:36:25 +0530

    Add make option in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,38 +2,55 @@
 
 usage()
 {
-        echo "Usage   : $0 [ OPTIONS ]"
-        echo "Options :  "
-        echo "     -s : Setup"
+	echo "Usage   : $0 [ OPTIONS ]"
+	echo "Options :  "
+	echo "     -s : Setup"
+	echo "     -m : Make"
 }
 
 if [ $# -eq 0 ]
 then
-        usage
-        exit
+	usage
+	exit
 fi
 
 while getopts "s" opt ;do
-        case "${opt}" in
-                s)
-						echo "Installing Intel PIN"
-						URL=https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.7-97619-g0d0c92f4f-gcc-linux.tar.gz
+	case "${opt}" in
+		s)
+			echo "Installing Intel PIN"
+			URL=https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.7-97619-g0d0c92f4f-gcc-linux.tar.gz
 
-						wget $URL -O pin.tar.gz 
-						tar -xvf pin.tar.gz
-
-						#Install Ubuntu Dependencies
-						sudo apt-get install gcc-multilib g++-multilib libc6-dev-i386
-						
-						#Rename pin directory
-						mv pin-* pin
-						
-						export PIN_ROOT=pin/
-						make
-						;;
-				*)
-						echo "Invalid Option"
-						usage
-						;;
-		esac
+			wget $URL -O pin.tar.gz 
+			tar -xvf pin.tar.gz
+			rm pin.tar.gz
+			#Install Ubuntu Dependencies
+			sudo apt-get install gcc-multilib g++-multilib libc6-dev-i386
+			#Rename pin directory
+			mv pin-* pin
+			mkdir out/
+			cd src
+			export PIN_ROOT=../pin/
+			make
+			mv obj-intel64/ ../out
+			echo "=================================================="
+			echo "                 Setup Completed                  "
+			echo "=================================================="
+			echo " Now run with : pin/pin -t obj/maintrace.so -o <logfile> -- <file to be traced>"
+			;;
+		m) 
+			rm -rf out/
+			cd src 
+			export PIN_ROOT=../pin/
+			make
+			mv obj-intel64/ ../out
+			echo "=================================================="
+			echo "                 Make Completed                  "
+			echo "=================================================="
+			echo " Now run with : pin/pin -t obj/maintrace.so -o <logfile> -- <file to be traced>"
+			;;
+		*)
+			echo "Invalid Option"
+			usage
+			;;
+	esac
 done
